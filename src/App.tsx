40992ec2cb0aa261b89b361e7044ed7e9895d293
Andrei Nascimento <andrei.lima.nas@outlook.com>
2022-10-04 19:43:16 -0300

    Acrescentando pagina de cadastro e rotas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
 
 function App() {
@@ -15,11 +16,12 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
           </Routes>
         </div>
       <Footer />
     </ BrowserRouter >
-    )
+  )
 }
 
 export default App;
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,31 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { login } from '../../services/Service';
+import UserLogin from '../../models/UserLogin';
 import './Login.css';
 
 function Login() {
+  let navigate = useNavigate();
+  const [token, setToken] = useLocalStorage('token');
+  const [userLogin, setUserLogin] = useState<UserLogin>({
+    id: 0,
+    nome: '',
+    usuario: '',
+    senha: '',
+    foto: '',
+    token: ''
+  })
+
+function updatedModel(event: ChangeEvent<HTMLInputElement>) {
+  setUserLogin({
+    ...userLogin,
+    [event.target.name]: event.target.value
+  })
+}
+
+async function conectar(event: ChangeEvent<HTMLFormElement>) {
+  event.preventDefault();
+  try {
+    await login(`usuarios/logar`, userLogin, setToken)
+
+    alert('Usuário logado com sucesso!');
+  } catch (error) {
+    alert('Dados do usuário inconsistentes. Erro ao logar!')
+  }
+}
+
+useEffect(() => {
+  if(token !== '') {
+    navigate('/home')
+  }
+}, [token])
+
   return (
     <Grid container direction='row' justifyContent='center' alignItems='center'>
       <Grid alignItems='center' xs={6}>
         <Box paddingX={15}>
-          <form>
+          <form onSubmit={conectar}>
             <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos'>Entrar</Typography>
-            <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-            <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+
+            <TextField value={userLogin.usuario} onChange={(event: ChangeEvent<HTMLInputElement>) => updatedModel(event)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+
+            <TextField value={userLogin.senha} onChange={(event: ChangeEvent<HTMLInputElement>) => updatedModel(event)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
 
             <Box marginTop={2} textAlign='center'>
-              <Link to='/home' className='text-decorator-none'>
                 <Button type='submit' variant='contained' className='logar'>
                   Logar
                 </Button>
-              </Link>
             </Box>
           </form>
+
           <Box display='flex' justifyContent='center' marginTop={2}>
             <Box marginRight={1}>
               <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
             </Box>
+            <Link to='/cadastrousuario'>
               <Typography variant='subtitle1'gutterBottom align='center' className='textos'>Cadastre-se</Typography>
+            </Link>
+              
           </Box>
         </Box>
       </Grid>
